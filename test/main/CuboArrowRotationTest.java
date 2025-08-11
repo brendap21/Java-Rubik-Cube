40fe322ab7bd4ee29d7147176838d6d8d1d65dc6
Brenda Navarro <105397988+brendap21@users.noreply.github.com>
2025-08-10 18:10:28 -0600

    Refactor arrow rotation mapping

--- test/main/CuboArrowRotationTest.java
+++ test/main/CuboArrowRotationTest.java
@@ -59,6 +59,55 @@ public class CuboArrowRotationTest {
     }
 
     @Test
+    public void testArrowRotationAllFacesAfterRotation() throws Exception {
+        // Apply a combination of rotations and ensure mapping holds for all faces
+        applyRot.invoke(cubo, 0, 30.0); // around X
+        applyRot.invoke(cubo, 1, 45.0); // around Y
+        Object[][] cases = new Object[][]{
+            // front face (1)
+            {1, new double[]{0, -1, 0}, 0, 0},
+            {1, new double[]{0, 1, 0}, 0, 1},
+            {1, new double[]{-1, 0, 0}, 1, 1},
+            {1, new double[]{1, 0, 0}, 1, 0},
+            // back face (0)
+            {0, new double[]{0, -1, 0}, 0, 1},
+            {0, new double[]{0, 1, 0}, 0, 0},
+            {0, new double[]{-1, 0, 0}, 1, 0},
+            {0, new double[]{1, 0, 0}, 1, 1},
+            // left face (4)
+            {4, new double[]{0, -1, 0}, 0, 0},
+            {4, new double[]{0, 1, 0}, 0, 1},
+            {4, new double[]{-1, 0, 0}, 1, 1},
+            {4, new double[]{1, 0, 0}, 1, 0},
+            // right face (5)
+            {5, new double[]{0, -1, 0}, 0, 1},
+            {5, new double[]{0, 1, 0}, 0, 0},
+            {5, new double[]{-1, 0, 0}, 1, 0},
+            {5, new double[]{1, 0, 0}, 1, 1},
+            // top face (3)
+            {3, new double[]{0, -1, 0}, 2, 0},
+            {3, new double[]{0, 1, 0}, 2, 1},
+            {3, new double[]{-1, 0, 0}, 2, 1},
+            {3, new double[]{1, 0, 0}, 2, 0},
+            // bottom face (2)
+            {2, new double[]{0, -1, 0}, 2, 0},
+            {2, new double[]{0, 1, 0}, 2, 1},
+            {2, new double[]{-1, 0, 0}, 2, 0},
+            {2, new double[]{1, 0, 0}, 2, 1}
+        };
+
+        for (Object[] c : cases) {
+            int face = (Integer) c[0];
+            double[] arrow = (double[]) c[1];
+            int expAxis = (Integer) c[2];
+            int expCw = (Integer) c[3];
+            int[] res = call(arrow, face);
+            assertEquals("axis for face " + face, expAxis, res[0]);
+            assertEquals("cw for face " + face, expCw, res[1]);
+        }
+    }
+
+    @Test
     public void testArrowRotation() throws Exception {
         // expected mapping: face index, arrow vector, axis, cw flag
         Object[][] cases = new Object[][]{
